d6f177e29fee865c24b9e0ab9865cde8ebfeed4a
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-04-26 11:24:28 +0700

    Add options in FrappeUI plugin.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ import './style.css'
 setConfig('resourceFetcher', frappeRequest)
 let app = createApp(App)
 app.use(VueQueryPlugin)
-app.use(FrappeUI)
+app.use(FrappeUI, {
+    resources: true,
+    call: true,
+    // TODO: during dev, I always get socket.io error.
+    // TODO: So I will disable this for now.
+    // TODO: I need to test socketio connection when this app is embedded in a frappe app.
+    socketio: false,
+})
 app.use(router)
 app.mount('#app')
